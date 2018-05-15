efc322c51a558a80e68dfcfdf43462ef47b7808b
Rickard Hammarén <rickard.hammaren@scilifelab.se>
2018-05-15 08:41:55 +0200

    inital commit for clean_bed

---
+++ POPGEN/clean_bed.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+basename=1$
+
+
+module load bioinfo-tools
+module load plink
+
+
+grep -P "\tI" $basename>> variants_to_remove
+grep -P  "\tD" $basename>> variants_to_remove
+
+grep -P "\tA\tT" $basename>> variants_to_remove
+grep -P "\tT\tA" $basename>> variants_to_remove
+grep -P "\tC\tG" $basename>> variants_to_remove
+grep -P "\tG\tC" $basename>> variants_to_remove
+
+plink --bfile $basename --exclude variants_to_remove --make-bed --out ${$basename}_cleaned~
+
