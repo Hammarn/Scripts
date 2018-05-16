7844d2976885da8f06198ff205a6b3b4a4961f30
Rickard Hammarén <rickard.hammaren@scilifelab.se>
2018-05-16 14:37:30 +0200

    correct string concatenation

--- POPGEN/IBS.r
+++ POPGEN/IBS.r
@@ -6,10 +6,11 @@
 args = commandArgs(trailingOnly=TRUE)
 if (length(args)==0) {
       stop("At least one argument must be supplied (input file).n", call.=FALSE)
+}    
 
 data1<- read.table (args[1], header = T)
 data2<-sort(data1$PI_HAT, decreasing = TRUE)
 
 plot(data2, cex=0.8, pch = 20, col="aquamarine4")
 title("IBS_proportion")
-dev2bitmap("IBS_proportion_args[1].pdf", type="pdfwrite")
+dev2bitmap(paste("IBS_proportion_",args[1],".pdf", sep=""), type="pdfwrite")
