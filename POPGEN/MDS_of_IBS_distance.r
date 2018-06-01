9f80537fc6942753b14bf7ba57971ccce35abf8f
Rickard Hammarén <rickard.hammaren@scilifelab.se>
2018-06-01 16:38:23 +0200

    syntax fix

--- POPGEN/MDS_of_IBS_distance.r
+++ POPGEN/MDS_of_IBS_distance.r
@@ -1,7 +1,7 @@
 args = commandArgs(trailingOnly=TRUE)
 if (length(args)==0) {
   stop("At least one argument must be supplied (input file).n", call.=FALSE)
-
+}
 inputfile<-"args[1]"
 WD<-getwd()
 setwd (WD)
