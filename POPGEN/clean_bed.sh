8b5480f2eb05470941b917989ddaf6bef903caa7
Rickard Hammarén <rickard.hammaren@scilifelab.se>
2018-06-05 15:51:50 +0200

    fluf

--- POPGEN/clean_bed.sh
+++ POPGEN/clean_bed.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 echo $1
-basename=$1
-
-
+basename=${1%.*}
+echo $basename
 module load bioinfo-tools
 module load plink
 
 
-grep -P "\tI" $basename>> variants_to_remove
-grep -P  "\tD" $basename>> variants_to_remove
+grep -P "\tI" ${basename}.bim >> variants_to_remove
+grep -P  "\tD" ${basename}.bim >> variants_to_remove
 
-grep -P "\tA\tT" $basename>> variants_to_remove
-grep -P "\tT\tA" $basename>> variants_to_remove
-grep -P "\tC\tG" $basename>> variants_to_remove
-grep -P "\tG\tC" $basename>> variants_to_remove
+grep -P "\tA\tT" ${basename}.bim >> variants_to_remove
+grep -P "\tT\tA" ${basename}.bim >> variants_to_remove
+grep -P "\tC\tG" ${basename}.bim >> variants_to_remove
+grep -P "\tG\tC" ${basename}.bim >> variants_to_remove
 
 plink --bfile $basename --exclude variants_to_remove --make-bed --out ./${basename}_cleaned
 
