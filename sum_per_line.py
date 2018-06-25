482f5cb22798fec4e3ae9539135f323cf32124f3
Rickard Hammarén <rickard.hammaren@scilifelab.se>
2018-06-25 08:35:32 +0200

    Script for getting percentages instead of counts from RFMIX output

---
+++ sum_per_line.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import re
+import os
+import sys
+
+input_file = sys.argv[1]
+with open('input_file', 'r') as f:
+    next(f)    
+    read_data = f.read()
+
+output_lines=[]
+
+for line in read_data:
+    line_list = line.split(" ")
+    s = sum(line_list[1:])
+    o = []
+    for i in line_list 
+    o.append() 
