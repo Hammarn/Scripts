6068c37e14a1d1c2089e9ea0341adf052cdc04f4
Rickard Hammarén <rickard.hammaren@scilifelab.se>
2018-06-25 11:22:42 +0200

    first working version

--- POPGEN/sum_per_line.py
+++ POPGEN/sum_per_line.py
@@ -4,7 +4,13 @@ import re
 import os
 import sys
 import pdb
+
 input_file = sys.argv[1]
+try:
+    output_file = input_file.replace('.txt','') + ".percentages.txt" 
+except:
+    output_file = input_file + ".percentages.txt"
+
 with open(input_file, 'r') as f:
     read_data = f.readlines()
 
@@ -20,8 +26,12 @@ output_dict={}
 for item in data:
     s_ = sum(data[item]) 
     output_dict[item]=[]
-    for i_ in data[item]: 
+    for i_ in data[item]:
+        #Only the denominator needs to be float to get a float as output
         output_dict[item].append(i_/float(s_))
 
-pdb.set_trace()
-print output_dict
+output=str()
+with open(output_file, 'w') as f:
+    for indv in original_order:
+        f.write(indv +' '+ ' '.join(str(p) for p in output_dict[original_order[0]]) + '\n')
+
