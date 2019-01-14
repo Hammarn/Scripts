c6e58485a4636317922788381b57903a6487b721
Rickard Hammarén <rickard.hammaren@scilifelab.se>
2019-01-14 12:10:06 +0100

    Now each pop is it's own series

--- POPGEN/plot_flash_pca.py
+++ POPGEN/plot_flash_pca.py
@@ -1,18 +1,38 @@
 #!/usr/bin/env python
+import argparse
 import pandas as pd
-import bokeh
+#import bokeh
+from bokeh.plotting import figure, show, output_file
+from bokeh.core.properties import value
+from bokeh.palettes import all_palettes
+from bokeh.models import Legend
+#from bokeh.models import ColumnDataSource
+import pdb
 
 def read_input(input_file):
     pd_data = pd.read_csv(input_file, sep = "\s+" )
     return pd_data
 
 
+def plotting(data):
+    #data = ColumnDataSource(data)
+    pops = list(pd.Series.unique(data['FID'])) 
+    color =  all_palettes['Inferno'][256] 
+
+    fig = figure(title="PCA", toolbar_location=None, x_axis_label='PCA 1',y_axis_label='PCA 2') 
+    for counter,pop in enumerate(pops):
+    #    data.loc[data['FID'] == pop]
+        fig.circle(x = 'PC1', y = 'PC2', legend = 'FID' ,color =color[counter],   source = data.loc[data['FID'] == pop] ,  muted_alpha=0.2)    
+        
+    fig.legend.label_text_font_size = "8px" 
+    fig.legend.click_policy="mute"
+    show(fig, browser = "firefox")
 if __name__ == "__main__":
     # Command line arguments
     parser = argparse.ArgumentParser("Make a scatter plot of FPKM counts between conditions")
-    parser.add_argument("-i", "--input", default = 'pca.txt',
-help="Output pca.txt file from flashpca")
-
+    parser.add_argument("-i", "--input", default = 'pcs.txt',
+help="Plots PCA from flashca")
 
 args = parser.parse_args()
 data = read_input(args.input)
+plotting(data)
