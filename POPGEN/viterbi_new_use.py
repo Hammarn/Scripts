7b609575fc891b0d2e467955cca1d2dfe1733d8c
Rickard Hammarén <rickard.hammaren@scilifelab.se>
2019-06-26 19:13:04 +0200

    writing out FB SNPs to file, no filtering though

--- POPGEN/viterbi_new_use.py
+++ POPGEN/viterbi_new_use.py
@@ -3,6 +3,7 @@ import sys
 import re
 import pdb
 import os
+import copy
 import argparse
 import pandas as pd 
 import numpy as np
@@ -87,7 +88,7 @@ def filter_away_telemomers(count_dict, bp_dict):
         ## pop instead of making a new library
     #    print "Filtering away telomeric regions for chr {} this could take a while".format(vit_num) 
         for key in bp_dict[vit_num].keys():
-          #  pdb.set_trace()
+            #  pdb.set_trace()
             if int(bp_dict[vit_num][key]) < first or int(bp_dict[vit_num][key])  > last:
                 try:
                     count_dict[vit_num].pop(key)
@@ -96,7 +97,7 @@ def filter_away_telemomers(count_dict, bp_dict):
     return count_dict
 
 
-def plotting(count_dict, bp_dict, FB_dict):
+def plotting(count_dict, bp_dict, FB_dict, backup_dict, print_FB):
     count_dict = count_dict
     bp_dict = bp_dict
     # Each key is a CHR
@@ -126,81 +127,96 @@ def plotting(count_dict, bp_dict, FB_dict):
                 'Khoisan' : pd.Series(Khoisan, index = Pos),
                  }#'Chromosome' : key }
             data_dict[key] = pd.DataFrame(data_dict[key])    
-### Actual plotting here
     data =  pd.concat([data_dict[key] for key in  data_dict.keys()])
     #p.xaxis.axis_label = 'Genomic position'
     #p.yaxis.axis_label = 'Genome proportion'
    
-    import pdb
-    pdb.set_trace()
     #FB_dict
     ## EXTRACTING stuff
     ## each row is one SNP in both viterbi and FB
     ## Find interesting viterbi rows
     ## return the corresponding rows in FB, should be fine to print for now.
     ## Threshold? if proporion CEU is < 95%??
+    if print_FB:
+        # Every fourth column in the dataframe
+        EUR_dict={}
+        #for i in range(1,3):
+        for i in range(1,23):
+            fourth=FB_dict[i].columns[::4]
+            print"Looking through SNP on chr{}".format(i)
+            EUR_dict[i]={}
+            for SNP in  backup_dict[i].keys():
+                # Not 100% EUR
+                if backup_dict[i][SNP][0] < 0.95:
+                    
+                    ## SNP name == every fourth probability
+                    EUR_dict[i][SNP]= FB_dict[i].loc[[SNP],fourth].to_string(index=False, header=False)
 
+        with open("viterbi_FB.txt", 'w') as f:
+            print"printing SNP with less than 95% primary ancestry to file"
+            for i in range(1,23):
+                for SNP in EUR_dict[i].keys():
+                    f.write("{} {} {}\n".format(i,SNP,EUR_dict[i][SNP]))
+
+                #backup_dict[i][]
+                #FB_dict[i]
     ## need to add in 
 
-    subplot()
-    
-    def subplot():
-        ## Make sure we are accessing the chromosomes in the correct order
-        for key in range(1,23):
-            print "Building subplot {}".format(key)
-            TOOLTIPS=[
-                #( "index", "$index"      ),
-                ( 'X',   '$x'            ),
-                ( 'Y',   '$y'            ),
-            ]
-            local_vars = vars()
-            local_vars['p{}'.format(key)] = figure(plot_height=500, plot_width=500, output_backend="webgl", title="Chromosome {}".format(key))
-            local_vars['p{}'.format(key)].circle(x = data_dict[key].index.values, y = data_dict[key]['CEU'], color = "grey"   )
-            local_vars['p{}'.format(key)].circle(x = data_dict[key].index.values, y = data_dict[key]['CDX'], color = "skyblue"  )
-            local_vars['p{}'.format(key)].circle(x = data_dict[key].index.values, y = data_dict[key]['YRI'], color = "goldenrod"  )
-            local_vars['p{}'.format(key)].circle(x = data_dict[key].index.values, y = data_dict[key]['Khoisan'], color = "salmon"  )
-            local_vars['p{}'.format(key)].xaxis.major_label_orientation = "vertical"
-            local_vars['p{}'.format(key)].xaxis[0].formatter.use_scientific = False
-            local_vars['p{}'.format(key)].xaxis[0].ticker = [int(data_dict[key].first_valid_index()) ,int(data_dict[key].last_valid_index())]
-            local_vars['p{}'.format(key)].x_range = Range1d(int(data_dict[key].first_valid_index()) ,int(data_dict[key].last_valid_index()))
+### Actual plotting here
+    print "Not plotting"
+    sys.exit() 
+    ## Make sure we are accessing the chromosomes in the correct order
+    for key in range(1,23):
+        print "Building subplot {}".format(key)
+        TOOLTIPS=[
+            #( "index", "$index"      ),
+            ( 'X',   '$x'            ),
+            ( 'Y',   '$y'            ),
+        ]
+        local_vars = vars()
+        local_vars['p{}'.format(key)] = figure(plot_height=500, plot_width=500, output_backend="webgl", title="Chromosome {}".format(key))
+        local_vars['p{}'.format(key)].circle(x = data_dict[key].index.values, y = data_dict[key]['CEU'], color = "grey"   )
+        local_vars['p{}'.format(key)].circle(x = data_dict[key].index.values, y = data_dict[key]['CDX'], color = "skyblue"  )
+        local_vars['p{}'.format(key)].circle(x = data_dict[key].index.values, y = data_dict[key]['YRI'], color = "goldenrod"  )
+        local_vars['p{}'.format(key)].circle(x = data_dict[key].index.values, y = data_dict[key]['Khoisan'], color = "salmon"  )
+        local_vars['p{}'.format(key)].xaxis.major_label_orientation = "vertical"
+        local_vars['p{}'.format(key)].xaxis[0].formatter.use_scientific = False
+        local_vars['p{}'.format(key)].xaxis[0].ticker = [int(data_dict[key].first_valid_index()) ,int(data_dict[key].last_valid_index())]
+        local_vars['p{}'.format(key)].x_range = Range1d(int(data_dict[key].first_valid_index()) ,int(data_dict[key].last_valid_index()))
+
+        #output_file("Rf_chr{}.html".format(key))
+        #export_svgs(local_vars['p{}'.format(key)], filename="Rfmix_intro_{}.svg".format(key))
+        #save(local_vars['p{}'.format(key)])
             
-
-            #output_file("Rf_chr{}.html".format(key))
-            #export_svgs(local_vars['p{}'.format(key)], filename="Rfmix_intro_{}.svg".format(key))
-            #save(local_vars['p{}'.format(key)])
-                
-        
-        plot_list = [local_vars['p{}'.format(i)] for i in data_dict.keys()]
-        #Make one figure out of the 22 subplots
-        p =gridplot(plot_list, ncols=10) 
-        #p = row(plot_list)
-       
-         
-
-        output_file("Rfmix_introgression.html")
-        print "Saving to output - this could take a while.."
-        save(p)
+    
+    plot_list = [local_vars['p{}'.format(i)] for i in data_dict.keys()]
+    #Make one figure out of the 22 subplots
+    p =gridplot(plot_list, ncols=10) 
+    #p = row(plot_list)
+   
+    output_file("Rfmix_introgression.html")
+    print "Saving to output - this could take a while.."
+    save(p)
 
 if __name__ == "__main__":
     # Command line arguments
     parser = argparse.ArgumentParser("Make a scatter plot of FPKM counts between conditions")
     parser.add_argument("-v", "--viterbi", nargs = '+',
-help="Viterbi output files from RFMix. Needs to contain the substring 'chrxx' where xx is the chromosome number in the filename")
+        help="Viterbi output files from RFMix. Needs to contain the substring 'chrxx' where xx is the chromosome number in the filename")
     parser.add_argument("-b", "--bim", nargs = '+',
-help="Bim file with genomic positions. Needs to contain the substring 'chrxx' where xx is the chromosome number in the filename")
+        help="Bim file with genomic positions. Needs to contain the substring 'chrxx' where xx is the chromosome number in the filename")
     parser.add_argument("-fb", "--forward-backwards", nargs = '+',
-help="ForwardBackward file from RFMix. Needs to contain the substring 'chrxx' where xx is the chromosome number in the filename")
+        help="ForwardBackward file from RFMix. Needs to contain the substring 'chrxx' where xx is the chromosome number in the filename")
+    parser.add_argument("-PFB", "--print_FB", default=True,
+    help="If FB positions should be revealed")
 
     args = parser.parse_args()
     viterbi_dict =  handle_input_files(args.viterbi) 
     bim_dict = handle_input_files(args.bim)
 
     FB_dict = handle_input_files(args.forward_backwards)
-    import pdb
-    pdb.set_trace()
 
     count_dict = OrderedDict()
-    FB_dict
     for chr_num in range(1,23): #22 chr
     #chr_num = 1  #22 chr
         print "Reading results for chr {}".format(chr_num)
@@ -215,8 +231,8 @@ help="ForwardBackward file from RFMix. Needs to contain the substring 'chrxx' wh
     #chr_num = 1  #22 chr
         print "Reading bim file for chr {}".format(chr_num)
         bp_dict[chr_num] = read_genetic_bim(bim_dict[str(chr_num)])
-    
+    backup_dict = copy.deepcopy(count_dict) 
     count_dict  =  filter_away_telemomers(count_dict, bp_dict) 
     
-    plotting(count_dict, bp_dict,FB_dict) 
+    plotting(count_dict, bp_dict,FB_dict, backup_dict,args.print_FB) 
     print "Goodbye"
