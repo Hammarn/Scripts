a70efd4fc70fd255612d3e270cb0e79c1ba51867
Rickard Hammarén <rickard.hammaren@scilifelab.se>
2019-10-23 11:10:59 +0200

    Mute on click

--- POPGEN/UMAP_plot.py
+++ POPGEN/UMAP_plot.py
@@ -17,40 +17,43 @@ from bokeh.palettes import inferno
 import pdb
 
 def read_input(input_file):
+    print "Reading data.."
     in_data = pd.read_csv(input_file, sep = "\s+" )
     return in_data
 
 def do_UMAP(data):
+    print "Performing dimensionality reduction"
     reducer = umap.UMAP()
     data_NO_index=data.drop(data.columns[[0,1]], axis=1)
     embedding = reducer.fit_transform(data_NO_index)
     return embedding
 
-def plotting(umaped_data, raw_data):
+def plotting(umaped_data, raw_data, output):
+    print "Building plot"
     umaped_df =  pd.DataFrame(data=umaped_data)
 
     source = pd.concat([raw_data[['FID','IID']], umaped_df], axis=1)
     source.set_index('FID')
     source  = source.rename(columns={0: "x", 1: "y"})
     #pdb.set_trace()
-    p = figure()
+    p = figure(title="UMAP of first 10 PCs", toolbar_location="above", x_axis_label="UMAP 1",y_axis_label="UMAP 2",plot_width = 1500, plot_height = 1000)
     
     fids = source.FID.unique()
     colours = inferno(len(fids))
     leg_1 = []
     for counter,pop in enumerate(fids):
-            
-        leg_1.append(( pop, [p.circle(x='x', y='y', source=source.loc[source['FID'] == pop], size=10, color=colours[counter])] ))
+           
+        ## Legend takes "Text to print", figure object.
+        ## This is here being parsed through a list. Makes it easy to add additional legends later if I want to
+        leg_1.append(( pop, [p.circle(x='x', y='y', source=source.loc[source['FID'] == pop], size=6, color=colours[counter], muted_alpha=0.2 ) ] ))
     
     legend1 = Legend(items=leg_1)#, location = (20, 20))
     p.add_layout(legend1, 'left') 
 
-
-    output_file('UMAP.html')
     #data.loc[data['Region'] == region]
    
 
-
+    p.legend.click_policy="mute"
     p.add_tools(HoverTool(
      tooltips = [
          ('Population', '@FID'),
@@ -58,7 +61,10 @@ def plotting(umaped_data, raw_data):
          ))
     
     
-    
+    outfile=output
+    print "Saving output to {}.html".format(outfile) 
+    output_file('{}.html'.format(outfile))
+   
     show(p)
     return
 
@@ -67,8 +73,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser("Take PCA eigenvectors from FlasPCA or similar, perform UMAP dimension reduction and plot the result")
     parser.add_argument("-i", "--input", default = 'pcs.txt',
     help="Name of inputfile")
+    parser.add_argument("-o", "--output", default = 'UMAP',
+    help="Name of outputfile")
 
 args = parser.parse_args()
 data = read_input(args.input)
 reduced = do_UMAP(data)
-plotting(reduced, data)
+plotting(reduced, data, args.output)
