2293214d6ff717f91721d97d903b627c6a7bce0b
Rickard Hammarén <rickard.hammaren@scilifelab.se>
2019-10-30 16:32:13 +0100

    added support for population grouping, requires a key file

--- POPGEN/UMAP_plot.py
+++ POPGEN/UMAP_plot.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import umap
 import warnings
+import sys
 warnings.filterwarnings('ignore')
 
 from bokeh.plotting import figure, output_file, show
@@ -28,24 +29,49 @@ def do_UMAP(data):
     embedding = reducer.fit_transform(data_NO_index)
     return embedding
 
-def plotting(umaped_data, raw_data, output):
+def plotting(umaped_data, raw_data, output, key_file):
+    key=key_file
     print "Building plot"
     umaped_df =  pd.DataFrame(data=umaped_data)
 
     source = pd.concat([raw_data[['FID','IID']], umaped_df], axis=1)
     source.set_index('FID')
     source  = source.rename(columns={0: "x", 1: "y"})
-    #pdb.set_trace()
     p = figure(title="UMAP of first 10 PCs", toolbar_location="above", x_axis_label="UMAP 1",y_axis_label="UMAP 2",plot_width = 1500, plot_height = 1000)
     
     fids = source.FID.unique()
     colours = inferno(len(fids))
     leg_1 = []
-    for counter,pop in enumerate(fids):
-           
-        ## Legend takes "Text to print", figure object.
-        ## This is here being parsed through a list. Makes it easy to add additional legends later if I want to
-        leg_1.append(( pop, [p.circle(x='x', y='y', source=source.loc[source['FID'] == pop], size=6, color=colours[counter], muted_alpha=0.2 ) ] ))
+    if key:
+        key_info = {}
+        try:
+            with open(key, 'r') as f:
+                for line in f:
+                    key_info[line.split()[0]] =  " ".join(line.split()[1:]) 
+        except  IOError:
+            print "Could not open the file '{}'".format(key)
+            sys.exit()
+
+        regions = set(key_info.values())
+        colours = inferno(len(regions))
+        
+        ki= (pd.Series(key_info)).to_frame()
+        ki = ki.sort_values(0)
+        ki_re = ki.reset_index()
+        ki_re = ki_re.rename(columns={'index':'FID', 0:'Region'})
+        source = pd.merge(source, ki_re, on = ['FID'])
+
+
+        for counter,region in enumerate(regions):
+            ## Legend takes "Text to print", figure object
+            ##todo make a list/DF with pops for each region so that can be used below:
+            leg_1.append(( region, [p.circle(x='x', y='y', source=source.loc[source['Region'] == region ], size=6, color=colours[counter], muted_alpha=0.2 ) ] ))
+    
+    else:
+        for counter,pop in enumerate(fids):
+            ## Legend takes "Text to print", figure object.
+            ## This is here being parsed through a list. Makes it easy to add additional legends later if I want to
+            leg_1.append(( pop, [p.circle(x='x', y='y', source=source.loc[source['FID'] == pop], size=6, color=colours[counter], muted_alpha=0.2 ) ] ))
     
     legend1 = Legend(items=leg_1)#, location = (20, 20))
     p.add_layout(legend1, 'left') 
@@ -57,6 +83,8 @@ def plotting(umaped_data, raw_data, output):
     p.add_tools(HoverTool(
      tooltips = [
          ('Population', '@FID'),
+         ('Region', '@Region'),
+         ('Individual', '@IID'),
              ]
          ))
     
@@ -70,13 +98,16 @@ def plotting(umaped_data, raw_data, output):
 
 if __name__ == "__main__":
     # Command line arguments
-    parser = argparse.ArgumentParser("Take PCA eigenvectors from FlasPCA or similar, perform UMAP dimension reduction and plot the result")
+    parser = argparse.ArgumentParser("Take PCA eigenvectors from FlashPCA or similar, perform UMAP dimension reduction and plot the result")
     parser.add_argument("-i", "--input", default = 'pcs.txt',
     help="Name of inputfile")
     parser.add_argument("-o", "--output", default = 'UMAP',
     help="Name of outputfile")
+    parser.add_argument("-k", "--key",
+    help="File with Population legend key")
+
 
 args = parser.parse_args()
 data = read_input(args.input)
 reduced = do_UMAP(data)
-plotting(reduced, data, args.output)
+plotting(reduced, data, args.output, args.key)
